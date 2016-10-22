8b98c62cd66ff4d8c60566c60fdf5b0097619018
bill8608 <bill8608@gmail.com>
2016-10-21 21:30:23 -0700

    add another method to reverseLinkedList.java

--- LinkedList/reverseLinkedList.java
+++ LinkedList/reverseLinkedList.java
@@ -46,3 +46,23 @@ public class Solution {
         return pre;
     }
 }
+
+//method 2 define tmp at the very beginning
+
+/*
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null;
+        while (head != null) {
+        ListNode tmp = head.next;
+        head.next = pre;
+        pre = head;
+        head = tmp;
+        }
+        return pre;
+    }
+}
+*/
