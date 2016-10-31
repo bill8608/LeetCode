ebe345197d3d09b7d1fa485b06cde75745647261
bill8608 <bill8608@gmail.com>
2016-10-30 23:20:12 -0700

    Sqrt.java

--- Math/Sqrt.java
+++ Math/Sqrt.java
@@ -1,5 +1,6 @@
 public class Sqrt {
 	/*
+	//This only works for x where (x/2)^2 < 2^31
     public int Sqrt(int x) {
         int start = 0, end = x;
         while (end - start > 1) {
@@ -13,15 +14,18 @@ public class Sqrt {
         return (end * end == x)? end:start;
     }
 	*/
+	
+	//Newton recursion
 	public int Sqrt(int x) {
-		double d = (double) x;
+		double d = (double) x; // target, to get SquareRoot of target
         double g = 1;
-        double e = 0.1;
+        double e = 0.1; // precision
         while (Math.abs(g - d/g) > e) {
             g = (g + d/g) / 2;
         }
         return (int) g;
     }
+	
 	
 	public static void main(String[] args) {
 		Sqrt sqrt1 = new Sqrt();
