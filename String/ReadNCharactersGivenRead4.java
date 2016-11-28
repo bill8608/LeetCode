dfedce81be5b274a515ae05a4b3c23a0fbabd3fe
bill8608 <bill8608@gmail.com>
2016-11-27 20:28:52 -0800

    ReadNCharactersGivenRead4.java

---
+++ String/ReadNCharactersGivenRead4.java
@@ -0,0 +1,69 @@
+/*
+157. Read N Characters Given Read4
+Total Accepted: 21640
+Total Submissions: 73547
+Difficulty: Easy
+Contributors: Admin
+The API: int read4(char *buf) reads 4 characters at a time from a file.
+
+The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
+
+By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+
+Note:
+The read function will only be called once for each test case.
+
+Hide Company Tags Facebook
+Hide Tags String
+Hide Similar Problems (H) Read N Characters Given Read4 II - Call multiple times
+*/
+
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+     /*
+    public int read(char[] buf, int n) {
+        //https://discuss.leetcode.com/topic/6400/accepted-clean-java-solution/2
+        char[] buffer = new char[4];
+        boolean endOfFile = false;
+        int readBytes = 0;
+        
+        while (readBytes < n && !endOfFile) {
+            int currReadBytes = read4(buffer);
+            if (currReadBytes != 4) {
+                endOfFile = true;
+            }
+            int length = Math.min(n - readBytes, currReadBytes);
+            for (int i = 0; i < length; i++) {
+                buf[readBytes + i] = buffer[i];
+            }
+            readBytes += length;
+        }
+        return readBytes;
+    }
+    */
+		// method 2
+        private int buffPtr = 0;
+        private int buffCnt = 0;
+        private char[] buff = new char[4];
+        public int read(char[] buf, int n) {
+            int ptr = 0;
+            while (ptr < n) {
+                if (buffPtr == 0) {
+                    buffCnt = read4(buff);
+                }
+                if (buffCnt == 0) break;
+                while (ptr < n && buffPtr < buffCnt) {
+                    buf[ptr++] = buff[buffPtr++];
+                }
+                if (buffPtr >= buffCnt) buffPtr = 0;
+            }
+            return ptr;
+    }
+}
